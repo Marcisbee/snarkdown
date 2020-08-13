6824ba3482149518f442f08c243cf1b19ef6663e
Marcis <marcisbergmanis@gmail.com>
2020-08-13 10:40:42 +0300

    1.3.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const TAGS = {
-	'' : ['<em>','</em>'],
-	_ : ['<strong>','</strong>'],
-	'~' : ['<s>','</s>'],
-	'\n' : ['<br />'],
-	' ' : ['<br />'],
+	'': ['<em>', '</em>'],
+	_: ['<strong>', '</strong>'],
+	'~': ['<s>', '</s>'],
+	'\n': ['<br />'],
+	' ': ['<br />'],
 	'-': ['<hr />']
 };
 
@@ -11,19 +11,19 @@ const TAGS = {
  *	@private
  */
 function outdent(str) {
-	return str.replace(RegExp('^'+(str.match(/^(\t| )+/) || '')[0], 'gm'), '');
+	return str.replace(RegExp('^' + (str.match(/^(\t| )+/) || '')[0], 'gm'), '');
 }
 
 /** Encode special attribute characters to HTML entities in a String.
  *	@private
  */
 function encodeAttr(str) {
-	return (str+'').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	return (str + '').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
 
 /** Parse Markdown into an HTML String. */
 export default function parse(md, prevLinks) {
-	let tokenizer = /((?:^|\n+)(?:\n---+|\* \*(?: \*)+)\n)|(?:^``` *(\w*)\n([\s\S]*?)\n```$)|((?:(?:^|\n+)(?:\t|  {2,}).+)+\n*)|((?:(?:^|\n)([>*+-]|\d+\.)\s+.*)+)|(?:\!\[([^\]]*?)\]\(([^\)]+?)\))|(\[)|(\](?:\(([^\)]+?)\))?)|(?:(?:^|\n+)([^\s].*)\n(\-{3,}|={3,})(?:\n+|$))|(?:(?:^|\n+)(#{1,6})\s*(.+)(?:\n+|$))|(?:`([^`].*?)`)|(  \n\n*|\n{2,}|__|\*\*|[_*]|~~)/gm,
+	let tokenizer = /((?:^|\n+)(?:\n---+|\* \*(?: \*)+)\n)|(?:^``` *(\w*)\n([\s\S]*?)\n```$)|((?:(?:^|\n+)(?:\t|  {2,}).+)+\n*)|((?:(?:^|\n)([>*+-]|\d+\.)\s+.*)+)|(?:\!\[([^\]]*?)\]\(([^\)]+?)\))|(\[)|(\](?:\(([^\)]+?)\))?)|(?:(?:^|\n+)([^\s].*)\n(\-{3,}|={3,})(?:\n+|$))|(?:(?:^|\n+)(#{1,3})\s*(.+)(?:\n+|$))|(?:`([^`].*?)`)|(  \n\n*|\n{2,}|__|\*\*|[_*]|~~)|((?:(?:^|\n+)(?:\|.*))+)/gm,
 		context = [],
 		out = '',
 		links = prevLinks || {},
@@ -31,17 +31,17 @@ export default function parse(md, prevLinks) {
 		chunk, prev, token, inner, t;
 
 	function tag(token) {
-		var desc = TAGS[token.replace(/\*/g,'_')[1] || ''],
-			end = context[context.length-1]==token;
+		var desc = TAGS[token.replace(/\*/g, '_')[1] || ''],
+			end = context[context.length - 1] == token;
 		if (!desc) return token;
 		if (!desc[1]) return desc[0];
-		context[end?'pop':'push'](token);
-		return desc[end|0];
+		context[end ? 'pop' : 'push'](token);
+		return desc[end | 0];
 	}
 
 	function flush() {
 		let str = '';
-		while (context.length) str += tag(context[context.length-1]);
+		while (context.length) str += tag(context[context.length - 1]);
 		return str;
 	}
 
@@ -50,7 +50,7 @@ export default function parse(md, prevLinks) {
 		return '';
 	}).replace(/^\n+|\n+$/g, '');
 
-	while ( (token=tokenizer.exec(md)) ) {
+	while ((token = tokenizer.exec(md))) {
 		prev = md.substring(last, token.index);
 		last = tokenizer.lastIndex;
 		chunk = token[0];
@@ -59,7 +59,7 @@ export default function parse(md, prevLinks) {
 		}
 		// Code/Indent blocks:
 		else if (token[3] || token[4]) {
-			chunk = '<pre class="code '+(token[4]?'poetry':token[2].toLowerCase())+'">'+outdent(encodeAttr(token[3] || token[4]).replace(/^\n+|\n+$/g, ''))+'</pre>';
+			chunk = '<pre class="code ' + (token[4] ? 'poetry' : token[2].toLowerCase()) + '">' + outdent(encodeAttr(token[3] || token[4]).replace(/^\n+|\n+$/g, '')) + '</pre>';
 		}
 		// > Quotes, -* lists:
 		else if (token[6]) {
@@ -68,12 +68,12 @@ export default function parse(md, prevLinks) {
 				token[5] = token[5].replace(/^\d+/gm, '');
 			}
 			inner = parse(outdent(token[5].replace(/^\s*[>*+.-]/gm, '')));
-			if (t==='>') t = 'blockquote';
+			if (t === '>') t = 'blockquote';
 			else {
 				t = t.match(/\./) ? 'ol' : 'ul';
 				inner = inner.replace(/^(.*)(\n|$)/gm, '<li>$1</li>');
 			}
-			chunk = '<'+t+'>' + inner + '</'+t+'>';
+			chunk = '<' + t + '>' + inner + '</' + t + '>';
 		}
 		// Images:
 		else if (token[8]) {
@@ -89,16 +89,38 @@ export default function parse(md, prevLinks) {
 		}
 		// Headings:
 		else if (token[12] || token[14]) {
-			t = 'h' + (token[14] ? token[14].length : (token[13][0]==='='?1:2));
-			chunk = '<'+t+'>' + parse(token[12] || token[15], links) + '</'+t+'>';
+			t = 'h' + (token[14] ? token[14].length : (token[13][0] === '=' ? 1 : 2));
+			chunk = '<' + t + '>' + parse(token[12] || token[15], links) + '</' + t + '>';
 		}
 		// `code`:
 		else if (token[16]) {
-			chunk = '<code>'+encodeAttr(token[16])+'</code>';
+			chunk = '<code>' + encodeAttr(token[16]) + '</code>';
 		}
 		// Inline formatting: *em*, **strong** & friends
 		else if (token[17] || token[1]) {
 			chunk = tag(token[17] || '--');
+		}
+		// Table parser
+		else if (token[18]) {
+			var l = token[18].split('\n'),
+				i = l.length,
+				table = '',
+				r = 'td>';
+			while (i--) {
+				if (l[i].match(/^\|\s+---+.*$/)) {
+					r = 'th>';
+					continue;
+				}
+				var c = l[i].split(/\|\s*/),
+					j = c.length,
+					tr = '';
+				while (j--) {
+					tr = (c[j] ? `<${r + parse(c[j])}</${r}` : '') + tr;
+				}
+				table = `<tr>${tr}</tr>` + table;
+				r = 'td>';
+			}
+			chunk = `<table>${table}</table>`;
 		}
 		out += prev;
 		out += chunk;
